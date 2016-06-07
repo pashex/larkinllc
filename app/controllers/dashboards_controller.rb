039ff14c4453e48ad1b31b4eef24b1425afca8d8
Pavel Golenkov <pashex@mail.ru>
2016-06-07 18:11:53 +0500

    Separate dates by completed loads for dispather

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,8 @@
 class DashboardsController < ApplicationController
   def dispatcher
     @dates = Order.order(:delivery_date).select(:delivery_date).distinct.pluck(:delivery_date)
+    @completed_dates = Load.where(completed: true).group(:delivery_date).having('COUNT(*) = 3').pluck(:delivery_date)
+    @uncompleted_dates = @dates - @completed_dates
   end
 
   def driver
